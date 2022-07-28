0adef2a4dbc9eeb862a87f9613b93d72dc6e3b30
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-28 19:20:27 +0000

    jueves

--- src/main.py
+++ src/main.py
@@ -9,13 +9,16 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config['DEBUG'] = True
+app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
+Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -15,5 +15,17 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        de.session.commit()
+
